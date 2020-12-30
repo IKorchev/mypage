529185fe88e00b0ffc8692cc8e18aaa00998bc66
Korchev <Korchev94@gmail.com>
2020-12-30 21:11:49 +0000

    some design tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,16 +17,6 @@ function Footer() {
                     <small className="d-block mt-2 text-muted">Copyright © 2020</small>
                     <small className="d-block mb-3 text-muted">Ivaylo Korchev</small>
                     </div>
-                    <form className="col-lg-2 py-2">
-                        <div className="form-group">
-                            <label for="exampleFormControlInput1">Email address</label>
-                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleFormControlTextarea1">Your message</label>
-                            <textarea class="form-control " placeholder="Message" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
-                    </form>
                 </div>
         </footer>
     )
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,8 +2,6 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../index.css'
 import { Link } from 'react-router-dom'
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-import {useSpring, animated } from 'react-spring'
 
 
 
@@ -18,20 +16,16 @@ function Home() {
         <>
         <div className="p-5 p-md-5  text-center bg-light shadow jumbo-image" >
             <div className="col-md-5 p-lg-2 mx-auto">
-                <h1 className="display-5 text-white"><b>Ivaylo Korchev</b></h1>
-                <p className="lead font-weight-normal text-white">Hello! Welcome to my page. My name is Ivaylo and I am an Aspiring React Developer. I have experience building apps with <strong className="stronk">React JavaScript Html CSS.</strong></p>
+                <h1 className="display-5 text-white" id="jtext"><b>Ivaylo Korchev</b></h1>
+                <p className="lead font-weight-normal text-white" id="jtext">Hello! Welcome to my page. My name is Ivaylo and I am an Aspiring React Developer. I have experience building apps with <strong className="stronk">React JavaScript Html CSS.</strong></p>
                 <br></br>
-                <p className="lead font-weight-normal text-white">Check out the <Link to="/Projects" id="p-link">Projects </Link> page for more information </p>
-                <div className="container d-flex justify-content-center">
-                    <a href="https://linkedin.com/" target="_blank" rel="noreferrer" className="icons mx-2"><FaLinkedin size={iconSize}></FaLinkedin></a>
-                    <a href="https://github.com/" target="_blank" rel="noreferrer" className="icons mx-2"><FaGithub size={iconSize}></FaGithub></a>
-                </div>
+                <p className="lead font-weight-normal text-white" id="jtext">Check out the <Link to="/Projects" id="p-link">Projects </Link> page for more information </p>
             </div>
         </div>
         <div className="container">
             <div className="row text-center justify-content-center">
-                <div className="col-md-3 p-4 mt-2">
-                <div className="card shadow-lg">
+                <div className="col-lg-3 p-4 mt-2">
+                    <div className="card shadow-lg">
                         <h4 className="card-header">First project</h4>
                         <div className="card-body">
                             <h1 className="card-title">About</h1>
@@ -45,7 +39,7 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3 p-4 mt-2">
+                <div className="col-lg-3 p-4 mt-2">
                 <div className="card shadow-lg">
                         <h4 className="card-header">Second project</h4>
                         <div className="card-body">
@@ -60,7 +54,7 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3 p-4 mt-2">
+                <div className="col-lg-3 p-4 mt-2">
                 <div className="card shadow-lg">
                         <h4 className="card-header">Third project</h4>
                         <div className="card-body">
