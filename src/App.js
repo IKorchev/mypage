8d4c764a3941f4250a712275b93cdd26a14370e9
Korchev <Korchev94@gmail.com>
2021-01-11 23:54:31 +0000

    trying new things

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Pages/home'
+import Projects from './Pages/projects'
+import Navbar from './components/navbar'
+import AboutMe from './Pages/aboutme'
+import './index.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbar />
+   <Home />
+   <Projects />
+   <AboutMe />
+   </>
   );
 }
 
